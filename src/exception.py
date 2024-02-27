529544160638115871da11f2dda1ed45f04a723b
Gopalakrishana-DS <gamebotics69@gmail.com>
2024-02-27 20:00:56 +0530

    done EDA on students data located in notebook folder

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -15,4 +16,4 @@ class CustomException(Exception):
 
 
     def __str__(str):
-        return self.error_message
+        return self.error_message 
